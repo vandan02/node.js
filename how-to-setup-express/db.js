d1329a342bf0d01bc3cec588280daf944698bc2d
vandan <vandanpatel493@gmail.com>
2024-10-23 15:41:50 +0530

    http

--- how-to-setup-express/db.js
+++ how-to-setup-express/db.js
@@ -1,4 +1,4 @@
-const mongoose=require ('mongoose');
+const mongoose=require('mongoose');
 const dbconnect =async ()=>{
     await mongoose.connect("mongodb+srv://vandanpatel493:vandan@cluster0.bm96r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     console.log("conected to server");
